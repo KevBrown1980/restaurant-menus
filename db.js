7b62876d23c0e4ce99b2683c12a48e816180014a
KevBrown1980 <kevinpatonbrown@gmail.com>
2023-02-15 21:42:33 +0000

    code updated - ready for testing

--- db.js
+++ db.js
@@ -1,7 +1,14 @@
 const path = require('path');
 const { Sequelize } = require('sequelize');
 
-// TODO - connect to db via sequelize
+// TODO - connect to db via 
+const sequelize = new Sequelize({
+    // Dialect needed
+    dialect: "sqlite",
+    // Where is the database located? __dirname is the current directory we're in
+    storage: path.join(__dirname, "db.sqlite")
+
+})
 
 module.exports = {
     sequelize
--- models/Menu.js
+++ models/Menu.js
@@ -2,5 +2,9 @@ const {sequelize} = require('../db');
 const { Sequelize } = require('sequelize');
 
 // TODO - create a Menu model
+let Menu = sequelize.define("menu", {
+    // Column names
+    title: Sequelize.STRING   
+})
 
 module.exports = {Menu};
