6eaa48e68a9c51eb17281d17199804df8e7cbd8b
KevBrown1980 <kevinpatonbrown@gmail.com>
2023-02-15 22:47:13 +0000

    manged to get 2 out 5 test working

--- db.js
+++ db.js
@@ -3,9 +3,9 @@ const { Sequelize } = require('sequelize');
 
 // TODO - connect to db via 
 const sequelize = new Sequelize({
-    // Dialect needed
+  
     dialect: "sqlite",
-    // Where is the database located? __dirname is the current directory we're in
+    
     storage: path.join(__dirname, "db.sqlite")
 
 })
--- index.test.js
+++ index.test.js
@@ -16,28 +16,33 @@ describe('Restaurant and Menu Models', () => {
         await sequelize.sync({ force: true });
     });
 
+//{ name: 'Burger King', location: 'Glasgow', cuisine: 'Burgers' }
+
     test('can create a Restaurant', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+        const restaurant1 = await Restaurant.create(seedRestaurant[0]);
+            expect(restaurant1.name).toBe('AppleBees');
+        
     });
 
     test('can create a Menu', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
+        const menu1 = await Menu.create({ title: 'Evening' });
+            expect(menu1.title).toBe('Evening');
     });
 
-    test('can find Restaurants', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
-    });
+    // test('can find Restaurants', async () => {
+    //     const restaurant22 = await Restaurant.create(seedRestaurant);
+    //     const restaurant23 = await Restaurant.findAll()
+    //     expect(restaurant23).toBe(seedRestaurant[1])
+    // });
 
-    test('can find Menus', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
-    });
+    // test('can find Menus', async () => {
+    //     // TODO - write test
+    //     expect('NO TEST').toEqual('EXPECTED DATA')
+    // });
 
-    test('can delete Restaurants', async () => {
-        // TODO - write test
-        expect('NO TEST').toEqual('EXPECTED DATA')
-    });
+    // test('can delete Restaurants', async () => {
+    //     const restaurant3 = await Restaurant.create({ name: 'McDonalds', location: 'Edinburgh', cuisine: 'Burgers and fries' });
+    //     let deletedRestaurant3 = restaurant3.destroy(); 
+    //     expect(deletedRestaurant3).toEqual({}); 
+    // });
 })
